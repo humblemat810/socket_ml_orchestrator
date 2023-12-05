3bf7abc8683f90a563066ce75b5b6789ae8b7210
humblemat810 <67593116+humblemat810@users.noreply.github.com>
2023-12-05 09:02:51 +0800

    feat: mixed worker .ini added, src file created

---
+++ src/pytaskqml/utils/sample/demo_case4_dispatcher.py
@@ -0,0 +1,130 @@
+
+import argparse
+parser = argparse.ArgumentParser(description="Demo server side for")
+parser.add_argument('--management-port', type=str, help='Management Port number', dest = 'management_port', default="8000")
+parser.add_argument('--log-level', dest="log_level", help='Configuration file path')
+parser.add_argument('--config', help='Configuration file path')
+parser.add_argument('--log-screen', action='store_const', const=True, default=False, help='Enable log to screen', dest="log_screen")
+parser.add_argument('--n-worker', help='num of workers', dest= "n_worker", type= int)
+args = parser.parse_args()
+
+import configparser
+config = configparser.ConfigParser()
+if args.config:    
+    config.read(args.config)
+
+management_port = config.get("dispatcher", "management-port")
+if management_port is None:
+    management_port = args.management_port
+management_port = int(management_port)
+log_level = config.get("logger", "level")
+if log_level is None:
+    log_level = args.log_level
+log_screen = config.get("logger", "logscreen")
+if log_screen is None:
+    log_screen = args.log_screen
+if type(log_screen) is str:
+    if log_screen.upper() == 'FALSE':
+        log_screen = False
+    elif log_screen.upper() == 'TRUE':
+        log_screen = True
+    else:
+        raise ValueError(f"incorrect config for log_screen, expected UNION[FALSE, TRUE], get {log_screen}")
+worker_config = []
+i = 1
+while f'worker.{i}' in config:
+    worker_section = config[f'worker.{i}']
+    location = worker_section['location']
+    location, port = location.split(':')
+    worker_config.append({"location": (str(location),int(port)),
+                          "min_start_processing_length": int(worker_section.get("min_start_processing_length"))
+                          })
+    i+=1
+    if args.n_worker is not None:
+        if i <= args.n_worker:
+            continue
+        else:
+            break
+if args.n_worker is not None:
+    while i <= args.n_worker:
+        port = str(int(port) + 1)
+        worker_config.append({"location": (str(location),int(port)),
+                          "min_start_processing_length": int(worker_section.get("min_start_processing_length"))
+                          })
+        i+=1
+
+import logging
+
+# Create a logger
+logger = logging.getLogger("demo_case1")
+logger.setLevel(log_level)
+
+# Create a console handler and set its format
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s - %(message)s",
+                              )
+
+console_handler.setFormatter(formatter)
+
+# Add the console handler to the logger
+if log_screen:
+    logger.addHandler(console_handler)
+fh = logging.FileHandler('demo_case1.log', mode='w', encoding='utf-8')
+fh.setLevel(log_level)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+import pytaskqml.task_dispatcher as task_dispatcher
+task_dispatcher.modulelogger = logger
+from dispatcher_side_demo_classes import my_word_count_socket_producer_side_worker, my_word_count_dispatcher
+import threading
+import time
+def main():
+    # worker_config = [#{"location": "local"},
+    #                  {"location": ('localhost', 12345), 
+    #                   "min_start_processing_length" : 42}, 
+    #                 ]
+    import sys
+    print(sys.modules[__name__])
+    my_task_worker_manager = my_word_count_dispatcher(
+                worker_factory=my_word_count_socket_producer_side_worker, 
+                worker_config = worker_config,
+                output_minibatch_size = 24,
+                management_port = management_port,
+                logger = logger,
+                retry_watcher_on = True
+            )
+    
+    th = threading.Thread(target = my_task_worker_manager.start, args = [])
+    th.start()
+
+    # this demo shows how to write own loop to dispatch data
+
+    logger.debug('signal start')
+    def dispatch_from_main():
+        # this function demonstrate main dispatch data such as video frame data or ref to video frame for example
+        cnt = 0
+        while not my_task_worker_manager.stop_flag.is_set():
+            cnt+=1
+            import random
+            words = ["apple", "banana", "cherry", "orange", "grape", "kiwi"]
+
+            def generate_random_word():
+                return random.choice(words)
+            frame_data = generate_random_word()#{"frame_number": cnt, "face": np.random.rand(96,96,6), 'mel': np.random.rand(80,16)}
+            my_task_worker_manager.dispatch(frame_data)
+            #time.sleep(0.01)
+            logger.debug(f'__main__ : cnt {cnt} added')
+
+    th = threading.Thread(target = dispatch_from_main, args = [], name='dispatch_from_main')
+    th.start()
+    while not my_task_worker_manager.stop_flag.is_set():
+        time.sleep(0.4)
+    while True:
+        try:
+            my_task_worker_manager.graceful_stopped.wait(timeout=2)
+        except:
+            continue
+        break
+if __name__ == '__main__':
+    main()
