b4c255ad8f0a6e6f31f1cc156e36481f6ae4aa5e
humblemat810 <67593116+humblemat810@users.noreply.github.com>
2023-12-16 02:38:44 +0800

    fix: move code inside main

--- src/pytaskqml/utils/sample/yappi_main_wrapper.py
+++ src/pytaskqml/utils/sample/yappi_main_wrapper.py
@@ -1,12 +1,3 @@
-import argparse
-wrapper_parser = argparse.ArgumentParser()
-wrapper_parser.add_argument('entrypoint',type = str,  help='pythonfile.main')
-wrapper_parser.add_argument('outfile_prefix',type = str,  help='prefix for outfile')
-args, remaining_args = wrapper_parser.parse_known_args()
-import sys
-print(sys.argv)
-sys.argv.pop(1)
-sys.argv.pop(1)
 
 def main():
     import importlib
@@ -38,6 +29,15 @@ def main():
     python yappi_main_wrapper.py demo_case1_dispatcher.py "--management-port", "18000", "--config", 'dispatcher_case2.ini'
     """
 if __name__ == '__main__':
+    import argparse
+    wrapper_parser = argparse.ArgumentParser()
+    wrapper_parser.add_argument('entrypoint',type = str,  help='pythonfile.main')
+    wrapper_parser.add_argument('outfile_prefix',type = str,  help='prefix for outfile')
+    args, remaining_args = wrapper_parser.parse_known_args()
+    import sys
+    print(sys.argv)
+    sys.argv.pop(1)
+    sys.argv.pop(1)
     import threading
     th = threading.Thread(target = main, name = args.outfile_prefix)
     th.start()
